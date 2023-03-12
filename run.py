af02c82ae6630e7203ebcb09a0370270500225ca
Dawesie <marie.dawe01967@gmail.com>
2023-03-12 11:04:39 +0000

    inital set up

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+def create-board():
+    pass
+
+def set_board():
+    pass
+
+def get_gesses():
+    pass
+
+def validate_guess():
+    pass
+
+
+
