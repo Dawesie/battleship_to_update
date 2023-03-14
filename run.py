857878877c71d17031841d90ae20801587291989
Dawesie <marie.dawe01967@gmail.com>
2023-03-14 19:23:49 +0000

    Add code to set up board

--- run.py
+++ run.py
@@ -1,7 +1,22 @@
 from random import randint
+import itertools
 
-def create-board():
-    pass
+
+name = input(f'Hello!\n Please enter your name: ')
+print(f'Your name is {name}')
+
+print(f"Welcome to My Battleship Admiral {name}.\n Your mission:\n To locate and destroy all of the enemies battleships \n before your torpedoes run out")
+
+
+def create_board():
+    """
+    This function sets up the grid and ransomly positons the battleships. 
+
+    """
+    grid = list(itertools.product(range(3), range(3)))
+    print(grid)
+    matrix = "\n".join([" ".join(["0" for x in range(3)]) for y in range(3)])
+    print(matrix)
 
 def set_board():
     pass
@@ -13,4 +28,4 @@ def validate_guess():
     pass
 
 
-
+create_board()
