246b093ce2e32a65d098c0a40f72a73b47313f15
Dawesie <marie.dawe01967@gmail.com>
2023-03-18 14:54:02 +0000

    add code for board to be an array, and imported array.  Add code to store ship positions in an array.

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from array import *
 
 name = input(f'Hello!\n Please enter your name: ')
 
@@ -7,38 +7,30 @@ name = input(f'Hello!\n Please enter your name: ')
 print(f"Welcome to My Battleship Admiral {name}.\n Your mission:\n To locate and destroy all of the enemies battleships \n before your torpedoes run out")
 
 
-def create_board():
+def create_board(data):
     """
-    This function sets up a matrix from which the grid for the game will be used. 
+    This function sets up a grid on which the game will be palyed.
 
     """
-    matrix = "\n".join([" ".join(["0" for x in range(3)]) for y in range(3)])
-    return matrix
+    grid_size = data
+    grid = [['o']*grid_size for x in range(grid_size)] 
+    for r in grid:
+        for c in r:
+            print(c,end = " ")
+        print()
+    return grid
 
-def position_ships(row, col):
+
+
+def position_ships(board):
     """
     To determin the row and column positions of the battle ships. 
     """
-    ship_row = row
-    ship_col = col
-    for x, y in zip(range(3),range(3)):
-        x = randint(0,3)
-        y = randint(0,3)
-        ship_row.append(x)
-        ship_col.append(y)
-    return ship_row, ship_col
-
-def duplicate_check(row, col):
-    """
-    Check to ensure there are no duplicate postions for the battleships or positon guessses. 
-    """
-    ship_row = row
-    ship_col = col
-    num = 0
-    for x, y in zip(range(3),range(3)):
-        if ship_row[num] and ship_col[num] = ship_row [num + 1] and ship_col[num + 1]:
-           x = randint(0,3)
-           ship_row.append[num] = x  
+    ship_board = board
+    for x in range(3):
+        y = randint(0,2)
+        ship_board[x][y] = 'S'
+    return ship_board
 
 
 
@@ -50,10 +42,9 @@ def validate_guess():
     pass
 
 
-board = create_board()
+grid_size = 3
+board = create_board(grid_size)
 print(board)
-ship_row = []
-ship_col =[]
-position_ships(ship_row, ship_col)
-print(ship_row, ship_col)
+ship_grid = position_ships(board)
+print(ship_grid)
 
